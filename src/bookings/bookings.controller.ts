0a2ed6c3836c7f11cbf54102b569532b8547c88d
YannickZahinda <ymulikuza@gmail.com>
2025-02-22 23:23:22 +0200

    fix the update feature

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ForbiddenException, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, NotFoundException, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dtos/create-booking.dto';
 import { JwtAuthGuard } from 'src/auth/guards/JwtGuard/jwt.auth.guard';
@@ -30,9 +30,14 @@ export class BookingsController {
         return this.bookingService.findAll();
     }
 
-    @Patch(':id')
-    updateStatus(@Param('id') id: number, @Body() status: { status: 'pending' | 'confirmed' | 'canceled' }) {
-        return this.bookingService.updateStatus(id, status.status);
+    @Patch(':id/status')
+    async updateStatus(@Param('id') id: number, @Body() status: { status: 'pending' | 'confirmed' | 'canceled' }) {
+        const updatedBooking = await this.bookingService.updateStatus(id, status.status);
+        if(!updatedBooking){
+            throw new NotFoundException(`Booking with ID ${id} not found`);
+        }
+
+        return updatedBooking;
     }
 
     @Get('user/:userId')
